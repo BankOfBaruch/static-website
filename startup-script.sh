2117b7029175742d6af44716f1090b86e6ea8384
BankOfBaruch <158302147+BankOfBaruch@users.noreply.github.com>
2025-08-13 13:38:19 -0400

    Update startup-script.sh

--- startup-script.sh
+++ startup-script.sh
@@ -4,6 +4,5 @@ apt-get update
 apt-get install -y nginx
 
 # Overwrite the default Nginx welcome page with our portfolio files
-# Replace YOUR_BUCKET_NAME with the name of your bucket from Step 2
 gsutil cp gs://[YOUR_BUCKET_NAME]/index.html /var/www/html/
 gsutil cp gs://[YOUR_BUCKET_NAME]/style.css /var/www/html/
