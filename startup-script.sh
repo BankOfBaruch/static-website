d8a8bf4119164da1ed2df37ee915990ddba103a4
BankOfBaruch <158302147+BankOfBaruch@users.noreply.github.com>
2025-08-13 13:37:36 -0400

    Update startup-script.sh
    Removed sensitive information

--- startup-script.sh
+++ startup-script.sh
@@ -5,5 +5,5 @@ apt-get install -y nginx
 
 # Overwrite the default Nginx welcome page with our portfolio files
 # Replace YOUR_BUCKET_NAME with the name of your bucket from Step 2
-gsutil cp gs://portfolio-assets-banks-sandbox-project/index.html /var/www/html/
-gsutil cp gs://portfolio-assets-banks-sandbox-project/style.css /var/www/html/
+gsutil cp gs://[YOUR_BUCKET_NAME]/index.html /var/www/html/
+gsutil cp gs://[YOUR_BUCKET_NAME]/style.css /var/www/html/
